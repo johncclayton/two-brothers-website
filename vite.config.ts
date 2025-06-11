038e9544b05906b226accf0912d953ac1966822b
John Clayton <john@effective-flow.ch>
2025-06-11 08:25:53 +0200

    Might work on gh pages now.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isGitHubPages = process.env.NODE_ENV === 'production';
+const base = isGitHubPages ? '/two-brothers-website/' : '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base,
 });
