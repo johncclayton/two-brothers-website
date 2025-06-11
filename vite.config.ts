448354fcd9a3111d353151a6293a781f08aef771
John Clayton <john@effective-flow.ch>
2025-06-11 10:53:57 +0200

    Revert to old website URL.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const isGitHubPages = process.env.NODE_ENV === 'production';
-const base = isGitHubPages ? '/two-brothers/' : '/';
+const base = isGitHubPages ? '/two-brothers-website/' : '/';
 
 // fixed?
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/two-brothers/',
+  base : base,
 });
