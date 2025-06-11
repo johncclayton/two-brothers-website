3e029b6055cac3628deb998325e57ee9da0dad80
John Clayton <john@effective-flow.ch>
2025-06-11 10:45:17 +0200

    Adjust url.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const isGitHubPages = process.env.NODE_ENV === 'production';
-const base = isGitHubPages ? '/two-brothers-website/' : '/';
+const base = isGitHubPages ? '/two-brothers/' : '/';
 
 // fixed?
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/two-brothers-website/',
+  base : '/two-brothers/',
 });
