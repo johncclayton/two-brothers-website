e81433e3d06906d48aa7a1ac7a292ba5dac02192
John Clayton <john@effective-flow.ch>
2025-06-11 11:25:54 +0200

    Set to two-brothers.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const isGitHubPages = process.env.NODE_ENV === 'production';
-const base = isGitHubPages ? '/two-brothers-website/' : '/';
+const base = isGitHubPages ? '/two-brothers/' : '/';
 
 // fixed?
 // https://vitejs.dev/config/
