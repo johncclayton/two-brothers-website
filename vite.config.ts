d9ef8c2f67c58da83d3384ce6211ea26957b5666
John Clayton <john@effective-flow.ch>
2025-06-12 06:49:02 +0200

    Check again.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 const isGitHubPages = process.env.NODE_ENV === 'production';
 const base = isGitHubPages ? '/two-brothers/' : '/';
 
-// fixed?
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
